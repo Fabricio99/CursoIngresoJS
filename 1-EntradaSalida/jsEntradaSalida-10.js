0978b5bb29ecde40b709f963edcd109b76295414
Fabri <fabriciogutierrez99@outlook.com>
2019-02-07 17:05:52 -0300

    terminado 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importeIngresado;
+	var descuento;
+	var importeFinal;
+
+	importeIngresado=importe.value;
+	importeIngresado=parseInt(importeIngresado);
+	descuento=importeIngresado*25/100;
+	importeFinal=importeIngresado-descuento;
+
+	resultado.value=importeFinal;
 }
