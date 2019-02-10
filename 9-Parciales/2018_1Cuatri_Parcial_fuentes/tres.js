4612b7329520dd81538f47a4927cdb914ed5d184
Fabri <fabriciogutierrez99@outlook.com>
2019-02-10 14:49:45 -0300

    3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
+	var precio=prompt("Ingrese precio ");
+	precio=parseInt(precio);
+	var descuento1=prompt("ingrese descuento ");
+	var descuento2=precio*descuento1/100;
+	var final;
+	final=precio-descuento2;
 
+	elPrecioFinal.value=final;
 }
