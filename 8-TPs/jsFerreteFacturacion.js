89992b57405ea4b734f8bee243afd3576dc5fab4
Fabri <fabriciogutierrez99@outlook.com>
2019-02-10 14:27:01 -0300

    Facturacion y parcial

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,7 +5,18 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var numero1=document.getElementById(PrecioUno);
+	numero1=parseInt(numero1);
+	var numero2=document.getElementById(PrecioDos);
+	numero2=parseInt(numero2);
+	var numero3=document.getElementById(PrecioTres);
+	numero3=parseInt(numero3);
+	var resultado; 
+	resultado=numero1+numero2+numero3;
+	alert("El resultado es: "+resultado);
+
+
+
 }
 function Promedio () 
 {
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
+	var base=prompt("Ingresa la base ");
 
+	var altura=prompt("Ingresa la altura ");
+
+	Perímetro=(2*altura)+(2*base);
+
+	alert("El perímetro del rectangulo es "+Perímetro);
 }
