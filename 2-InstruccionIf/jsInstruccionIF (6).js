67071cba3209aa636c88e6b45c928c4e84190b22
fabri <fabriciogutierrez99@outlook.com>
2019-02-12 12:06:20 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+  var edadIngresada;
+  edadIngresada=edad.value;
+  edadIngresada=parseInt(edadIngresada);
 
-
-
-
+  if(edadIngresada>18)
+  {
+  	alert("No es adolescente");
+  }
 }//FIN DE LA FUNCIÓN
