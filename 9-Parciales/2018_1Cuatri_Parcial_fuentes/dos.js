4e5278df6d5ce9436b73cf8fef9e3ae55de9771d
alumno <alumno>
2019-02-13 09:30:38 -0300

    P1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,10 +1,22 @@
 function mostrar()
 {
-  var nombre;
+  var precio;
+  var aumento; 
+  var final;
+
+  precio = prompt("ingrese precio");
+  precio = parseInt(precio);
+  aumento = precio * 10/100;
+  final = precio + aumento;
+
+  alert("El precio del producto es "+precio+" con tarjeta tiene un recargo del 10% que seria "+aumento+" costandole un final de "+final);
+
+  
+  /*var nombre;
   var localidad;
 
-  nombre=elNombre.value;
-  localidad=laLocalidad.value;
+  nombre = elNombre.value;
+  localidad = laLocalidad.value;
 
-  alert("Usted se llama "+nombre+" y vive en "+localidad);
+  alert("Usted se llama "+nombre+" y vive en "+localidad);*/
 }
