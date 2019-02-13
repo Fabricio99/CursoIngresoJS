d224e7d9bb9cb0ee493617f99093fd50a715b0e9
alumno <alumno>
2019-02-13 09:13:45 -0300

    P1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,11 +1,12 @@
 function mostrar()
 {
-	var precio=prompt("Ingrese precio ");
-	precio=parseInt(precio);
-	var descuento1=prompt("ingrese descuento ");
-	var descuento2=precio*descuento1/100;
-	var final;
-	final=precio-descuento2;
+	var precio = prompt("Ingrese precio ");
+	precio = parseInt(precio);
 
-	elPrecioFinal.value=final;
+	var descuento1 = prompt("ingrese descuento ");
+	var descuento2 = precio*descuento1/100;
+	var final;
+	final = precio - descuento2;
+
+	elPrecioFinal.value = final;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,11 +1,21 @@
 
 function mostrar()
 {
-	var base=prompt("Ingresa la base ");
+    var nombre1 = prompt("Nombre y apellido1");
+    var nombre2 = prompt("Nombre y apellido2")
+    var edad1 = prompt("Edad1");
+    edad1 = parseInt(edad1);
+    var edad2 = prompt("Edad2");
+    edad2 = parseInt(edad2);
 
-	var altura=prompt("Ingresa la altura ");
+    resultado = edad1 + edad2;
 
-	Perímetro=(2*altura)+(2*base);
+    alert("Ustedes son "+nombre1+" y "+nombre2+" y sus edades son "+edad1+" y "+edad2+" y la suma de sus edades es "+resultado);   
+	/*var base = prompt("Ingresa la base ");
 
-	alert("El perímetro del rectangulo es "+Perímetro);
+	var altura = prompt("Ingresa la altura ");
+
+	Perímetro = (2*altura) + (2*base);
+
+	alert("El perímetro del rectangulo es "+Perímetro);*/
 }
