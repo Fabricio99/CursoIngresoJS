dd9011737378f2def06ba18c4c6d909fb0bfa10f
Fabricio <fabriciogutierrez99@outlook.com>
2019-02-24 18:25:24 -0300

    P

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,9 +4,17 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 
+	acumulador = prompt("ingrese 5 numeros");
+	acumulador = parseInt(contador);
+
+	while(acumulador>5)
+	{
+		acumulador = prompt("ingrese 5 numeros");
+	}
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+
+    document.getElementById('suma').value=acumulador;
+    document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -6,25 +6,61 @@ function mostrar()
 //si el paquete es "todoincluido" te suma un 15% y para el resto de los paquetes no tiene descuento adicional
 
 {
-	var tarjeta;
-	var descuentoVisa;
-	var recargo;
-	var efectivo;
 	var medioDePago;
-	var suma;
 	var precioFinal;
-	var mensaje;
 	var precio;
+	var paquete;
 
  	precio=prompt("Elija el precio");
  	medioDePago=prompt("Elija medio de pago");
+ 	paquete=prompt("Puede elegir paquete todo incluido o solo desayuno");
 
  	precio=parseInt(precio);
 
- 	descuentoVisa=precio*10/100
-    
+ 	switch(medioDePago)
+ 	{
+ 		case "tarjeta visa":
+ 		case "mercado pago":
+ 		descuento=10;
+ 		    break;
 
+ 		case "paypal":
+ 		    switch(paquete)
+ 		    {
+ 		    	case "todoincluido":
+ 		    	descuento=25;
+ 		    	    break;
 
+ 		    	default:
+ 		    	descuento=15;
+ 		    	     break;
+ 		    }
+ 		    break;
 
-	alert(mensaje);
+ 		case "efectivo":
+ 		    switch(paquete)
+ 		    {
+ 		    	case "todoincluido":
+ 		    	descuento=35;
+ 		    	    break;
+
+ 		    	case "solodesayuno":
+ 		    	descuento=30;
+ 		    	    break;
+
+ 		    	default:
+ 		    	descuento=20;
+ 		    	    break;
+ 		    }
+ 		    break;
+
+ 		default:
+ 		descuento=5;
+ 		    break;
+ 	}
+
+ 	precioFinal=precio*descuento/100;
+ 	precioFinal=precio - descuento;
+
+ 	alert("Precio final de la compra: $"+precioFinal);
 }
